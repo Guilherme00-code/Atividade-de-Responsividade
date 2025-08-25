af09dfb83ea5d2c771fc0cc071bf327a38e71998
Guilherme00-code <Guilherme2008.fl@gmail.com>
2025-08-25 13:48:43 -0300

    parte do footer

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll to sections
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
